9fe0ec359e92659d9d2043dc0ad13409df0496e7
SleyDev <lyes.ord@gmail.com>
2024-07-21 19:17:05 +0200

    wip: sugar class config

--- src/Sucre.ts
+++ src/Sucre.ts
@@ -1,4 +1,10 @@
 export class Sucre {
+  static UnSucre: Sucre = new Sucre(1);
+  static DeuxSucres: Sucre = new Sucre(2);
+  static TroisSucres: Sucre = new Sucre(3);
+  static QuatreSucres: Sucre = new Sucre(4);
+  static CinqSucres: Sucre = new Sucre(5);
+
   private readonly _amountOfSugar: number;
 
   getSugarAmount(): number {
@@ -11,6 +17,23 @@ export class Sucre {
 
   DonneLeMaxDeSucreDisponible() {
     return new Sucre(this.getSugarAmount());
+  }
+
+  static Parse(sugar: number) {
+    switch (sugar) {
+      case 1:
+        return Sucre.UnSucre;
+      case 2:
+        return Sucre.DeuxSucres;
+      case 3:
+        return Sucre.TroisSucres;
+      case 4:
+        return Sucre.QuatreSucres;
+      case 5:
+        return Sucre.CinqSucres;
+      default:
+        throw new Error();
+    }
   }
 
   public toString() {
